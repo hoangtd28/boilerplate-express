a816a576abf9c74afd783e883fba1a0eda215a7b
hoangtd28 <102499847+hoangtd28@users.noreply.github.com>
2022-03-29 09:06:50 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 app.listen(3000);
-app.get ('/',(req, res) {
+app.listen(3000);
+app.get ("/", function(req, res) {
   res.send("Hello Express");
 });
-
 
 
 
