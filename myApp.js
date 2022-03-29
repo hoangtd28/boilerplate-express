88e9f91b298eb9b4f300002ffb5bcfdf5f146cac
hoangtd28 <102499847+hoangtd28@users.noreply.github.com>
2022-03-29 09:33:28 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static(__dirname + '/public/style.css'));
 app.get ("/", function(req, res) {
    res.sendFile(__dirname + '/views/index.html')
 });
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
