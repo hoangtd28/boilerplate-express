d5d37ae510469af6e22a120850136adb361e7d30
hoangtd28 <102499847+hoangtd28@users.noreply.github.com>
2022-03-29 13:45:29 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var app = express();
+
+app.use(function(req, res, next) {
+  console.log(req.method, req.path,"-", req.ip);
+  next();
+});
+
+function getTime(){
+  return new Date().toString();
+}
+app.get('/now', function(req, res, next) {
+  req.time = getTime()
+  next();
+}, function(req, res) {
+  res.json({time: req.time});
+});
+
+app.get('/:word/echo',function(req,res){ res.json({echo: req.params.word})});
+
 app.get ("/", function(req, res) {
    res.sendFile(__dirname + '/views/index.html')
 });
+
 app.use('/public', express.static(__dirname + '/public'));
-app.get(app.get ("/json", function(req, res) {
-   res.json({"message": "Hello json"} );
-}));
+
+app.get ("/json", function(req, res) {
+   if (process.env.MESSAGE_STYLE==='uppercase'){
+     res.json({"message": "HELLO JSON"} );}else{
+     res.json({"message": "Hello json"});
+     }
+});
 
 
 
